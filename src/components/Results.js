e94e3d073543fa5c74d0cd922869bbe1272e578c
Sylvia Garcia-Chavez <sgarciachavez@gmail.com>
2019-05-14 23:11:36 -0500

    Completed Results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
 
 class Results extends Component{
+
   render(){
+    const { question, authedUser,users } = this.props
+    const answer = question.optionOne.votes.includes(authedUser)
+    ? "optionOne"
+    : "optionTwo"
+
+    const optionOneVotes = question.optionOne.votes.length
+    const optionTwoVotes = question.optionTwo.votes.length
+    const totalVotes = optionOneVotes + optionTwoVotes
+    const stat1 = optionOneVotes/totalVotes * 100
+    const stat2 = optionTwoVotes/totalVotes * 100
+
+
     return (
-      <div>Results!</div>
+      <div className="results-item-container">
+         <div className="question-header">
+          <span className='bold-purple'>Asked by  </span>
+          {users[question.author].name}
+        </div>
+         <div className="question-avatar">
+           <img
+             className='avatar'
+             src={users[question.author].avatarURL}
+             alt={users[question.author].name} />
+         </div>
+         <div className="question-main">
+           <p className='bold-purple'>Results</p>
+           <div className={answer === "optionOne" ? "result-item-select" : "result-item"}>
+            {answer === "optionOne" && <div className="topright">Your vote!</div>}
+            <p>Would you rather {question.optionOne.text}?</p>
+            <ProgressBar now={stat1} label={`${stat1}%`} />
+            {optionOneVotes} out of {totalVotes}
+          </div>
+           <div className={answer === "optionTwo" ? "result-item-select" : "result-item"}>
+            {answer === "optionTwo" && <div className="topright">Your vote!</div>}
+            <p>Would you rather {question.optionTwo.text}?</p>
+           <ProgressBar now={stat2} label={`${stat2}%`} />
+           {optionTwoVotes} out of {totalVotes}
+           </div>
+         </div>
+      </div>
+
     )
   }
 }
 
-export default Results
+function mapStateToProps({authedUser, users, questions},props){
+  const { question_id } = props.match.params
+  const question = questions ? questions[question_id] : null
+  return{
+    question: question,
+    authedUser: authedUser,
+    users: users,
+  }
+}
+
+export default connect(mapStateToProps)(Results)
