35736fa5059de976513f16550506769a85674a5b
Sylvia Garcia-Chavez <sgarciachavez@iMac.attlocal.net>
2020-06-16 17:37:03 -0500

    Updates

---
+++ precache-manifest.019b5a8dfdabd1cd3120778e6ab9de5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451686d9f5fa267eac53e174e1d36ea2",
+    "url": "/WouldYouRather/index.html"
+  },
+  {
+    "revision": "0a1606b000f9a03fd5d1",
+    "url": "/WouldYouRather/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cf930edbd24a68a57d69",
+    "url": "/WouldYouRather/static/css/main.ceeb87f4.chunk.css"
+  },
+  {
+    "revision": "0a1606b000f9a03fd5d1",
+    "url": "/WouldYouRather/static/js/2.98493cec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WouldYouRather/static/js/2.98493cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf930edbd24a68a57d69",
+    "url": "/WouldYouRather/static/js/main.721ae689.chunk.js"
+  },
+  {
+    "revision": "68a0093b97cf18261ad0",
+    "url": "/WouldYouRather/static/js/runtime-main.ebd9032a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WouldYouRather/static/media/logo.5d5d9eef.svg"
+  }
+]);
